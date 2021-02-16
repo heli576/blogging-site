aff58d1e799adabd42d50f9b7afca0955ef92b32
heli576 <helivakharia@gmail.com>
2021-02-16 21:50:51 +0530

    Added update blog route

--- controllers/blog.js
+++ controllers/blog.js
@@ -7,7 +7,7 @@ exports.blogById=(req,res,next,id)=>{
   .exec((err,blog)=>{
     if(err||!blog){
       return res.status(400).json({
-        error:errorHandler(err)
+        error:"Blog does'nt exists."
       });
     }
     req.blog=blog;
@@ -32,4 +32,20 @@ blog.save((error,data)=>{
   }
   res.json(data);
 })
+};
+
+exports.updateBlog=(req,res)=>{
+  Blog
+    .findOneAndUpdate({_id:req.params.blogId}, req.body)
+    .exec(function(err, blog){
+      if(err) return res.status(500).json({err: err.message});
+      //res.json({blog, message: 'Successfully updated'})
+    });
+    Blog.findById(req.params.blogId)
+    .exec((err,blog)=>{
+      if(err){
+        console.log(err);
+      }
+      res.json(blog);
+    })
 };
--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const router=express.Router();
 const {requireSignin,isAuth,userById}=require("../controllers/user");
-const {createBlog,blogById}=require("../controllers/blog");
+const {createBlog,blogById,updateBlog}=require("../controllers/blog");
 
 
 router.post("/blog/:userId",requireSignin,isAuth,createBlog);
+router.put("/blog/:blogId/:userId",requireSignin,isAuth,updateBlog);
 
 router.param("userId",userById);
 router.param("blogId",blogById);
